bb6ee630ac9c9e8094e80a26223bb0b935d3c2d2
kaushal-analyst <kaushalk.tops@gmail.com>
2025-11-18 10:59:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def home():
     return "Hello Tops! 🚀 Welcome to DevOps with Python, Docker, Jenkins & Kubernetes."
 
-if __name__ == '__main__':
+if __name__ == '__master__':
     app.run(host='0.0.0.0', port=5000)
